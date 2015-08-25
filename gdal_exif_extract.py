928b03b748db9e1e71e91984eda69129bad3603d
Daljit Singh <singhdj2@agron-88-242.dhcp.ksu.edu>
2015-08-25 11:37:38 -0500

    gdal_exif_extract.py

---
+++ gdal_exif_extract.py
@@ -0,0 +1,17 @@
+__author__ = 'singhdj2'
+
+#GDAL based program to access the image and Geotiff exif information.
+
+from osgeo import gdal
+gjpeg = gdal.Open( "/Users/singhdj2/Desktop/IMG_4887.JPG" )
+
+meta_dict = gjpeg.GetMetadata()
+
+#print type(meta_dict)
+#print dir(meta_dict)
+
+for tag, value in meta_dict.items():
+    print tag, ':', value
+
+
+
